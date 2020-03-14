4743af3312de5df1dc2b0e0a6005dd185efa1dc4
arslan.ismail <arslanismail840@gmail.com>
2020-03-15 02:41:43 +0500

    Development Ready Application Setup

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router> 
+       <div className="App">
+         <header>
+           <Link to="/">Home</Link>
+           <Link to="/otherpage">Other Page</Link>
+         </header>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+
+      </div>
+    </Router>
+
+  );
+}
+
+export default App;
